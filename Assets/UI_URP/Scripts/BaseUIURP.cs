0a179be66e9b34b52cd2059f7f9f8c4e663a7035
Loi <loidanghn@gmail.com>
2021-08-04 09:22:18 -0500

    resolved conflict issue

--- Assets/UI_URP/Scripts/BaseUIURP.cs
+++ Assets/UI_URP/Scripts/BaseUIURP.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class BaseUI : MonoBehaviour
+public class BaseUIURP : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/UI_URP/Scripts/SettingsMenuURP.cs
+++ Assets/UI_URP/Scripts/SettingsMenuURP.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class SettingsMenuURP : BaseUI
+public class SettingsMenuURP : BaseUIURP
 {
     // Start is called before the first frame update
 
